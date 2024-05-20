aa3a95fc92d27454892b09f6bd807111d869d3e8
Robert Dixon <robmsc24@gmail.com>
2024-05-20 11:22:49 -0400

    wip

--- data/migrations/20240519075528_create_avengers_table.js
+++ data/migrations/20240519075528_create_avengers_table.js
@@ -1,10 +1,9 @@
 exports.up = function (knex) {
   return knex.schema.createTable("avengers", function (table) {
-    table.increments("id").primary();
-    table.string("name", 255).notNullable();
-    table.string("superpower", 255);
-    // Add the enemy column here with appropriate data type (likely string)
-    table.string("enemy", 255);
+    table.increments("id").primary(); // Auto-incrementing primary key
+    table.string("name", 255).notNullable(); // Name column, required
+    table.string("superpower", 255); // Superpower column
+    table.string("enemy", 255); // Enemy column
   });
 };
 
